edd7cb5805ea18eb613ecd7f69dfa8a0bc93133e
phasedchirp <sean.martin@nyu.edu>
2016-08-31 17:14:51 -0400

    Now with not-great web interface

---
+++ src (copy)/Main.hs
@@ -0,0 +1,19 @@
+import Snap
+import Control.Applicative ((<|>))
+
+main :: IO ()
+main = quickHttpServe site
+
+site :: Snap ()
+site =
+    ifTop (writeBS "hello world") <|>
+    route [ ("foo", writeBS "bar")
+          , ("echo/:echoparam", echoHandler)
+          ] <|>
+    dir "static" (serveDirectory ".")
+
+echoHandler :: Snap ()
+echoHandler = do
+    param <- getParam "echoparam"
+    maybe (writeBS "must specify echo/param in URL")
+          writeBS param
