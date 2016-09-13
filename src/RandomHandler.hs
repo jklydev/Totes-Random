6a75b5f510e589620e8121ba2bf2e455d2ba3760
phasedchirp <sean.martin@nyu.edu>
2016-09-12 22:18:35 -0400

    Did some clean-up

--- src/RandomHandler.hs
+++ src/RandomHandler.hs
@@ -9,14 +9,8 @@ import TwitSeed
 import Info
 import System.Random
 import System.Random.Mersenne.Pure64
-import Data.Aeson --(ToJSON(toJSON),encode,Value,FromJSON(..),Object(..),(.:))
+import Data.Aeson (ToJSON(toJSON),FromJSON(..),Value(Object),(.:),encode,decode)
 import Network.HTTP.Conduit
-
-
-
--- unpackTweet :: Response String -> String
--- unpackTweet (Request _ _ _ xs) = xs
-
 
 
 data TweetEmbed = TweetEmbed {html :: String } deriving (Show)
@@ -35,13 +29,6 @@ randVal = do
   -- manager <- newManager tlsManagerSettings
   requests <- sequence $ map getTweet tweets
   let requests' = map html $ maybe [] id $ sequence requests
-  -- let requests' = map (setRequestManager manager) requests
-  -- tweetResponse <- sequence $ map makeReq requests
-  -- let tweets' = map getBody tweetResponse
-  -- return $ show requests
-  -- tweetResponse <- sequence $ map ((fmap getResponseBody) .getTweet) tweets
-  -- tweets' <- sequence tweetResponse
-  -- let tweets' = map getTweet tweets
   let mersenne = pureMT $ fromIntegral twint
       randoCalinteger = fst $ randomInt mersenne
       randOfTheKing = fst $ randomR (0,1) (mkStdGen randoCalinteger) :: Double
--- src/TwitSeed.hs
+++ src/TwitSeed.hs
@@ -15,7 +15,6 @@ import qualified Data.Text.IO as T
 import Control.Monad.IO.Class (liftIO)
 import Control.Lens ((^.))
 import Control.Monad.Trans.Resource (runResourceT)
--- import Data.Char (ord)
 import Info -- twitter API credential
 import Data.Hashable
 import Data.Aeson (ToJSON(toJSON), object, (.=))
@@ -24,8 +23,8 @@ query :: APIRequest StatusesFilter StreamingAPI
 query = statusesFilter [Track ["random", "gaussian","cauchy","exponential","gamma","the hulk",
   "chi-squared","chai latte","wishart","correlation","implies","causality","stochastic",
   "probability","randos","entropy","totes random","serendipity","coincidence","surprise",
-  "bayes","baes","laplace","lapdance","unlikely","million to one","lottery","las vegas",
-  "monte carlo","monty python","MCMC","astrology","heart of gold"]]
+  "bayes","baes","laplace","unlikely","million to one","lottery","las vegas","distribution",
+  "monte carlo","monty python","MCMC","astrology","accidental","heart of gold"]]
 
 data TwitterSeed a = TwitterSeed {val::a , tweets::[String]}
 
@@ -42,11 +41,9 @@ twit = do
 testProc = do
   tweet <- await
   case tweet of (Just (SStatus status)) -> do
-                  --(liftIO . print) (status ^. statusText)
                   yield tweet
                   testProc
                 otherwise -> do
-                    -- (liftIO . return) ()
                     testProc
 
 makeURL :: (String, String) -> String
