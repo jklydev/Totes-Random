caf32c1aba59c8333fdfa2daacd9639ae2971156
phasedchirp <sean.martin@nyu.edu>
2016-09-21 15:04:20 -0400

    fixed index.html maybe?

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,14 @@ import Snap.Core
 import Snap.Util.FileServe
 import Snap.Http.Server
 import RandomHandler
+-- import GetInfo (getPortNum)
 import qualified Data.ByteString.Char8 as B
 import Control.Monad.IO.Class (liftIO)
 import System.IO (FilePath,readFile)
+-- import            Heist
+-- import            Heist.Interpreted
+-- import qualified  Data.Text as T
+-- import qualified  Text.XmlHtml as X
 
 main :: IO ()
 main = quickHttpServe site
@@ -54,3 +59,12 @@ echoHandler = do
     -- param <- getParam "echoparam"
     maybe (writeBS "failed to generate a random number")
           writeBS rVal
+
+-- portSplice :: Splice Snap
+-- portSplice = do
+--     port <- liftIO getPortNum
+--     let newURL = "http://totes-random.website/rand"
+--     -- input <- getParamNode
+--     -- let text = T.unpack $ X.nodeText input
+--         -- n = read text :: Int
+--     return [X.TextNode $ T.pack $ port]
